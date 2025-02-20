9c1ac0cd255c24a7abc6a4df1caf68eed921ba7b
ArtemOvsyannikov <artemrotfront@gmail.com>
2025-02-20 14:11:58 +0300

    make app

---
+++ android/app/src/main/kotlin/com/example/flutter_proj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum(int albumId) async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'),
+  );
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({required this.userId, required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'userId': int userId, 'id': int id, 'title': String title} =>
+        Album(userId: userId, id: id, title: title),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
+  int albumId = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum(albumId);
+  }
+
+  void _nextAlbum() {
+    setState(() {
+      albumId++;
+      futureAlbum = fetchAlbum(albumId);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Fetch Data Example')),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _nextAlbum,
+          tooltip: 'Следующий альбом',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
